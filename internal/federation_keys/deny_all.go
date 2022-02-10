db629cd05bdeadcdba46f98acba73f9caba45604
ALex Nikonov <alex@nikonov.tech>
2022-02-10 10:46:46 +0500

    chore: set license headers (#22)

--- internal/federation_keys/deny_all.go
+++ internal/federation_keys/deny_all.go
@@ -1,3 +1,7 @@
+// Copyright 2021 The Uranium Authors. All rights reserved.
+// Use of this source code is governed by a AGPL-style
+// license that can be found in the LICENSE file.
+
 package federation_keys
 
 type DenyAllKeystore struct{}
