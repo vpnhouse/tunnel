92b10356d06675a8d1530ae133775108305161d7
Artem Alemasov <artem.alemasov@gmail.com>
2022-12-01 15:20:51 +0800

    Fix migrations

--- internal/storage/db/migrations/00008-peer-stats.sql
+++ internal/storage/db/migrations/00008-peer-stats.sql
@@ -2,8 +2,8 @@
 -- +migrate Up
 -- +migrate StatementBegin
 ALTER TABLE "peers" ADD column "activity" INTEGER;
-ALTER TABLE "peers" ADD column "upstream" INTEGER DEFAULT 0;
-ALTER TABLE "peers" ADD column "downstream" INTEGER DEFAULT 0;
+ALTER TABLE "peers" ADD column "upstream" INTEGER NOT NULL DEFAULT 0;
+ALTER TABLE "peers" ADD column "downstream" INTEGER NOT NULL DEFAULT 0;
 -- +migrate StatementEnd
 
 -- +migrate Down
