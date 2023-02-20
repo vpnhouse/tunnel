91497b7b1dc37aa2bea5366da71a0ca9fb740405
Artem Alemasov <artem.alemasov@gmail.com>
2023-02-20 14:23:29 +0800

    69541 Fix timeouts

--- pkg/eventlog/client_timeouts.go
+++ pkg/eventlog/client_timeouts.go
@@ -1,10 +1,12 @@
 package eventlog
 
-import "time"
+import (
+	"time"
+)
 
 const (
-	reportOffsetTimeout   = 5 * time.Second
-	lockTtl               = time.Minute
+	reportOffsetTimeout          = 5 * time.Second
+	lockTtl                      = time.Minute
 	defaultLockProlongateTimeout = 30 * time.Second
 
 	waitOutputWriteTimeout = time.Second
