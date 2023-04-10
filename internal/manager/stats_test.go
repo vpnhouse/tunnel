bf0ee7be244959d0702706ae7f543fed0b176b3d
Artem Alemasov <artem.alemasov@gmail.com>
2023-04-10 17:24:05 +0800

    69541 Fix test

--- internal/manager/stats_test.go
+++ internal/manager/stats_test.go
@@ -3,24 +3,24 @@ package manager
 import (
 	"testing"
 	"time"
-	
+
 	"github.com/stretchr/testify/require"
 )
 
 func TestPeerSessions(t *testing.T) {
 	ts := time.Date(2023, 03, 01, 10, 0, 0, 0, time.UTC)
 	updateInterval := 5 * time.Minute
-	stat := newRuntimePeerStat(ts.Unix(), 0, 0)
+	stat := newRuntimePeerStat(ts.Unix(), 0, 0, "")
 
 	ts = ts.Add(time.Minute)
-	stat.Update(ts, 1, 1, updateInterval)
+	stat.Update(ts, 1, 1, "", updateInterval)
 	require.Equal(t, 1, len(stat.Sessions()))
-	
+
 	ts = ts.Add(time.Second)
-	stat.Update(ts, 2, 2, updateInterval)
+	stat.Update(ts, 2, 2, "", updateInterval)
 	require.Equal(t, 1, len(stat.Sessions()))
 
 	ts = ts.Add(time.Hour)
-	stat.Update(ts, 3, 3, updateInterval)
+	stat.Update(ts, 3, 3, "", updateInterval)
 	require.Equal(t, 2, len(stat.Sessions()))
 }
