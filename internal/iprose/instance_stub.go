45e0bb4a6c66a708d9d87b4a494770ab3e67f9fa
Sergey Kovalev <xray@randoman.ru>
2023-05-16 19:28:38 +0700

    Uncomment iprose stub

--- internal/iprose/instance_stub.go
+++ internal/iprose/instance_stub.go
@@ -3,26 +3,26 @@
 
 package iprose
 
-// import (
-// 	"github.com/go-chi/chi/v5"
-// 	"github.com/vpnhouse/tunnel/internal/authorizer"
-// )
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/vpnhouse/tunnel/internal/authorizer"
+)
 
-// type Instance struct {
-// 	stopped bool
-// }
+type Instance struct {
+	stopped bool
+}
 
-// func New(authorizer *authorizer.JWTAuthorizer) (*Instance, error) {
-// 	return &Instance{}, nil
-// }
+func New(authorizer *authorizer.JWTAuthorizer) (*Instance, error) {
+	return &Instance{}, nil
+}
 
-// func (instance *Instance) RegisterHandlers(r chi.Router) {}
+func (instance *Instance) RegisterHandlers(r chi.Router) {}
 
-// func (instance *Instance) Shutdown() error {
-// 	instance.stopped = true
-// 	return nil
-// }
+func (instance *Instance) Shutdown() error {
+	instance.stopped = true
+	return nil
+}
 
-// func (instance *Instance) Running() bool {
-// 	return !instance.stopped
-// }
+func (instance *Instance) Running() bool {
+	return !instance.stopped
+}
