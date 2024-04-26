6b2f32262a89e59cb0d17456a08862f452d2d557
Sergey Kovalev <xray@randoman.ru>
2024-04-26 12:59:11 +0700

    Remove explicit ciphers initialization

--- cmd/tunnel/main.go
+++ cmd/tunnel/main.go
@@ -187,22 +187,6 @@ func initServices(runtime *runtime.TunnelRuntime) error {
 		runtime.Services.RegisterService("certMaster", certMaster)
 		tlsCfg := &tls.Config{
 			GetCertificate: certMaster.GetCertificate,
-			CurvePreferences: []tls.CurveID{
-				tls.CurveP256,
-				tls.CurveP384,
-				tls.X25519,
-			},
-			CipherSuites: []uint16{
-				tls.TLS_AES_128_GCM_SHA256,
-				tls.TLS_AES_256_GCM_SHA384,
-				tls.TLS_CHACHA20_POLY1305_SHA256,
-				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-				tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-				tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
-				tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
-			},
 		}
 
 		// store the plaintext http router to use for
